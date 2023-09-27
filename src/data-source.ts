3633ef1f5be2dd8f9904fc9a7e7e1e816adb6a5c
Henrique Malvar <henriquemalvar@gmail.com>
2023-09-26 22:21:18 -0300

    feat: change to get dynamic models and migrations

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,34 +2,50 @@ import "dotenv/config";
 import { DataSource } from "typeorm";
 import * as fs from "fs";
 import * as path from "path";
-import { CreateVoteTable1695316369773 } from "./migrations/1695316369773-createVoteTable";
-import { CreateUserTable1695316370216 } from "./migrations/1695316370216-createUserTable";
-import { CreateRoomTable1695316370719 } from "./migrations/1695316370719-createRoomTable";
-import { CreateConfigTable1695316371173 } from "./migrations/1695316371173-createConfigTable";
 
-class DynamicEntity {
+class DynamicDataSource {
   dir: string;
   files: string[];
   entities: any[];
+  migrations: any[];
   constructor() {
     this.dir = "";
     this.files = [];
     this.entities = [];
+    this.migrations = [];
   }
 
-  public init(): any[] {
+  public async init(): Promise<any[]> {
     this.dir = path.join(__dirname, "./models");
     this.files = fs
       .readdirSync(this.dir)
       .filter((file) => file.endsWith(".ts") || file.endsWith(".js"));
 
-    this.entities = this.files.map((file) => {
-      const entity = require(path.join(this.dir, file));
-      return entity.default || entity;
-    });
+    await Promise.all(
+      this.files.map(async (file) => {
+        const entity = await import(path.join(this.dir, file));
+        this.entities.push(entity.default || entity);
+      })
+    );
+
+    this.dir = path.join(__dirname, "./migrations");
+    this.files = fs
+      .readdirSync(this.dir)
+      .filter((file) => file.endsWith(".ts") || file.endsWith(".js"));
+
+    await Promise.all(
+      this.files.map(async (file) => {
+        const migration = await import(path.join(this.dir, file));
+        this.migrations.push(migration.default || migration);
+      })
+    );
+
     return this.entities;
   }
 }
+
+const dynamicDataSource = new DynamicDataSource();
+dynamicDataSource.init();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
@@ -40,12 +56,7 @@ export const AppDataSource = new DataSource({
   database: process.env.DB_NAME,
   synchronize: true,
   logging: true,
-  entities: new DynamicEntity().init(),
+  entities: dynamicDataSource.entities,
   subscribers: [],
-  migrations: [
-    CreateUserTable1695316370216,
-    CreateConfigTable1695316371173,
-    CreateRoomTable1695316370719,
-    CreateVoteTable1695316369773,
-  ],
+  migrations: dynamicDataSource.migrations,
 });
